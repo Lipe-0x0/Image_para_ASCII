5e45e66ef84a2895bf8f2a71bdd4bb9a6e90cd46
Lipe-0x0 <luisfeli683@gmail.com>
2026-08-11 20:41:26 -0300

    Tranformei em função

--- Conversor.py
+++ Conversor.py
@@ -3,33 +3,39 @@ import matplotlib.pyplot as plt
 import matplotlib.image as img
 import cv2
 
-# Caracteres ASCII darker-lighter
-ascii = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'
 
-# Defina o caminho de onde está a imagem
-path = str(input("Defina o caminho de onde a imagem está: "))
+def img_ascii(path, escala_redimensionamento = 0.5):
+    '''
+    path = Caminho do arquivo
+    escala_redimensionamento = Valor que definirá o tamanho em que a imagem será redimensionada inde de 0-1,
+    sendo 0.5 padrão
 
-# Defina o formato da imagem
-formato_img = str(input("Formato da imagem (jpeg, png, ...): "))
+    Processos Aplicados: 
+    1 - Reamostragem por Interpolação (Interpolação por Área)
+    2 - Fórmula ITU-R BT.601 para transformar em preto e branco
+    
+    Decisões: 
+    Valor de pixel 0 = Preto (Utiliza caracteres robustos)
+    Valor de pixel 255 = Branco (Utiliza caracteres esparsos)
+    '''
 
-# Carregando Imagem
-array = img.imread(path, format = formato_img)
+    # Caracteres ASCII darker-lighter
+    ascii = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'
 
-# Redimensionando imagem para tamanho desejável utilizando processos de reamostragem(Interpolação)
-escala = float(input("Defina a escala que a imagem possuíra (0.5, 0.6, ..., 1): "))
+    # Formato da imagem (Pegando o ultimo elemento do path que é justamente o formato da imagem)
+    formato_img = path.split(".")[1]
 
-# Opencv(cv2) com método de interpolação por área
-array_redi = cv2.resize(array, None, fx = escala, fy = escala,  interpolation = cv2.INTER_AREA)
+    # Carregando Imagem
+    array = img.imread(path, format = formato_img)
 
-# Deixando preto e branco (Fórmula ITU-R BT.709)
-array_redi_709 = 0.2125*array_redi[:,:,0] + 0.7153*array_redi[:,:,1] + 0.0721*array_redi[:,:,2]
+    # Opencv(cv2) com método de interpolação por área
+    array_redi = cv2.resize(array, None, fx = escala_redimensionamento, fy = escala_redimensionamento,  interpolation = cv2.INTER_AREA)
 
-# Deixando preto e branco (Fórmula ITU-R BT.601)
-array_redi_601 = 0.299*array_redi[:,:,0] + 0.587*array_redi[:,:,1] + 0.114*array_redi[:,:,2]
+    # Deixando preto e branco (Fórmula ITU-R BT.601)
+    array_redi = 0.299*array_redi[:,:,0] + 0.587*array_redi[:,:,1] + 0.114*array_redi[:,:,2]
 
-# Salvando Imagem redimensioanada
-nome = "action1." + formato_img
-plt.imsave(nome, array_redi_709)
+    print(np.max(array_redi))
+    print(np.min(array_redi))
 
-nome = "action2." + formato_img
-plt.imsave(nome, array_redi_601)
+    return None
+
