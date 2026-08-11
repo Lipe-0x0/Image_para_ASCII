775f2811280199e38297598eed76484fa36b9f64
Lipe-0x0 <luisfeli683@gmail.com>
2026-08-11 19:26:23 -0300

    Redimensionando imagem e dps deixando em preto e branco

--- Conversor.py
+++ Conversor.py
@@ -1,14 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.imagem as img
+import cv2
 
 # Caracteres ASCII darker-lighter
 ascii = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'
 
+# Defina o caminho de onde está a imagem
+path = str(input("Defina o caminho de onde a imagem está"))
+
+# Defina o formato da imagem
+formato_img = str(input("Formato da imagem (jpg, png, ...): "))
+
 # Carregando Imagem
-image = img.imread("path", format = "jpg")
+array = img.imread(path, format = formato_img)
+
+# Redimensionando imagem para tamanho desejável utilizando processos de reamostragem(Interpolação)
+escala = int(input("Defina a escala que a imagem possuíra (0.5, 0.6, ..., 1): "))
+
+# Opencv(cv2) com método de interpolação por área
+array_redi = cv2.resize(array, fx = escala, fy = escala,  interpolation = cv2.INTER_AREA)
 
 # Deixando preto e branco (Fórmula ITU-R BT.709)
-array = 0.2125*image[:,:,0] + 0.7153*image[:,:,1] + 0.0721*image[:,:,2]
+array_redi = 0.2125*array_redi[:,:,0] + 0.7153*array_redi[:,:,1] + 0.0721*array_redi[:,:,2]
 
-
+plt.imshow(array_redi)
+plt.axis("off")
+plt.show()
